5665888ad6ab21e33615ebbec07b2f5d0dd34f02
Rodrigo Soares <silvars1@gmail.com>
2016-01-13 18:05:00 -0200

    Added file handling to the ruby script.

--- rss-epidemic-benchmark-trace-evaluator.rb
+++ rss-epidemic-benchmark-trace-evaluator.rb
@@ -1,7 +1,19 @@
 @path = ARGV.first
 
+def evaluate(string_line)
+  puts string_line
+end
+
+def valid_for_evaluation?(string_line)
+  string_line[0] == 'r' && string_line.include?('Payload')
+end
+
 def process_file(path)
-  puts path
+  File.open(path) do |trace_file|
+    trace_file.each do |line|
+      evaluate line if valid_for_evaluation?(line)
+    end
+  end
 end
 
 def check_file(path)
