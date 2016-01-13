9239287cbffa194fdf2e99ef2f94b6c23e9e235c
Rodrigo Soares <silvars1@gmail.com>
2016-01-13 19:07:02 -0200

    Updated Ruby script with a regex to extract trace informations.

--- rss-epidemic-benchmark-trace-evaluator.rb
+++ rss-epidemic-benchmark-trace-evaluator.rb
@@ -1,7 +1,10 @@
+TRACE_INFO_REGEX = /r\s(?<timestamp>\d+.?\d*).*DA=00:00:00:00:00:(?<dst>\d\d),\sSA=00:00:00:00:00:(?<src>\d\d).*1068\s(?<src_ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s>\s(?<dst_ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*Packet\sID:\s(?<packet_id>\d{8})\sHop\scount:\s(?<hop_count>\d{1,2})/
+
 @path = ARGV.first
 
-def evaluate(string_line)
-  puts string_line
+def evaluate(trace_line)
+  trace_info = trace_line.match TRACE_INFO_REGEX
+  puts "timestamp: #{trace_info[:timestamp]}, packet: #{trace_info[:packet_id]}, src: #{trace_info[:src]}, dst: #{trace_info[:dst]}, hops: #{trace_info[:hop_count]}, src ip: #{trace_info[:src_ip]}, dst ip: #{trace_info[:dst_ip]}"
 end
 
 def valid_for_evaluation?(string_line)
